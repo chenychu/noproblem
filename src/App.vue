c5d2ac6e9fba14f0927d27e569837bb908335ba9
chenychu <1093048320@qq.com>
2024-04-01 13:00:42 +0800

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
           this.$refs.loading.out();
         }
       }, 300);
-      fetch("127.0.0.1:5000")
+      fetch("http://127.0.0.1:5000")
         .then((v) => v.text())
         .then((v) => console.log(v));
     },
